b4f03a7ebeef9597d435aaa68134a70924818258
Nnaemeka Nnodim <mannynnodim@gmail.com>
2023-10-08 20:38:02 +0100

    features: calendar with react day picker

---
+++ app/(mentee)/mentee-booking/components/booking-session/Calender.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { DayPicker } from "react-day-picker";
+import "./calendar.css";
+
+interface ICalendarProps {
+  unAvailableDates?: Date[];
+  updateDate: (date: Date[] | Date | undefined) => void;
+}
+
+const Calendarcomponent = ({
+  unAvailableDates,
+  updateDate,
+}: ICalendarProps) => {
+  const updateDates = (dates: Date[] | Date | undefined) => {
+    updateDate(dates);
+  };
+
+  return (
+    <div className="max-w-[374px] border border-Accent1 rounded-xl">
+      <DayPicker
+        selected={unAvailableDates}
+        onSelect={(date) => updateDates(date)}
+        mode="multiple"
+        disabled={unAvailableDates}
+      />
+    </div>
+  );
+};
+
+export default Calendarcomponent;
---
+++ app/(mentee)/mentee-booking/components/booking-session/Radio.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import { check } from "@/public";
+
+interface RadioButtonProps {
+  label: string;
+  value: string;
+  selectedValue: string;
+  onSelect: (value: string) => void;
+}
+
+const RadioButton: React.FC<RadioButtonProps> = ({
+  label,
+  value,
+  selectedValue,
+  onSelect,
+}) => {
+  const isChecked = value === selectedValue;
+
+  return (
+    <label
+      htmlFor="radio"
+      className="flex gap-5 items-center cursor-pointer border border-black py-2 px-6 rounded-lg "
+    >
+      <p className="font-normal text-base">{label}</p>
+      <input
+        id="radio"
+        type="radio"
+        value={value}
+        checked={isChecked}
+        onChange={() => onSelect(value)}
+        className="hidden"
+      />
+      <div className="w-6 h-6 border rounded-full flex items-center justify-center border-gray-400 transition duration-300">
+        {isChecked && (
+          <Image src={check} alt="Checkmark" className="w-full h-full" />
+        )}
+      </div>
+    </label>
+  );
+};
+
+export default RadioButton;
