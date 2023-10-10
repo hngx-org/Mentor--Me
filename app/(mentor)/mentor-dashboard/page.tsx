ea66f622de8582e2043c80eed3b9f0913b38a818
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-10-10 15:43:03 +0100

    add layout component

--- app/(mentor)/mentor-dashboard/page.tsx
+++ app/(mentor)/mentor-dashboard/page.tsx
@@ -9,25 +9,21 @@ import Footer from "@/components/Footer";
 import MentorSideBar from "@/components/SideBar/MentorSideBar";
 import Modal from "@/components/mentor-dashboard/Modal";
 import MenteeNavBar from "@/components/menteeTopNav";
+import MobileSideBar from "@/components/mentor/MobileSiderBar";
 
 const page = () => {
   const [isVerified, setIsVerified] = useState<boolean>(true);
   const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
-    <div className="grid grid-cols-[auto,_minmax(0,_1fr)] grid-rows-[auto,_1fr]">
-      <MentorSideBar />
-
-      <div>
-        <MenteeNavBar />
-        <div className="w-full px-5 py-10 lg:p-20 bg-[#f9fafc]">
-          {!isVerified && <UnverifiedMentorCard />}
-          <MentorSession
-            isVerified={isVerified}
-            isOpen={isOpen}
-            setIsOpen={setIsOpen}
-          />
-          <AboutMentor />
-        </div>
+    <div className="">
+      <div className="px-5 py-10 lg:p-10 bg-[#f9fafc]">
+        {!isVerified && <UnverifiedMentorCard />}
+        <MentorSession
+          isVerified={isVerified}
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+        />
+        <AboutMentor />
       </div>
       <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
     </div>
