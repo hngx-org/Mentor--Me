938f59e6e1b567b08c089626bef9eedf93113ab9
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2023-10-10 17:19:14 +0100

    made it centered with the sidebar on very large screens

--- app/(mentee)/mentee-resources/Container.tsx
+++ app/(mentee)/mentee-resources/Container.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <section className="relative w-full max-w-[1440px] mx-auto">
+    <section className="!relative w-full max-w-[1536px] mx-auto">
       {children}
     </section>
   );
