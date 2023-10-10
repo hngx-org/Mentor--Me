68e0f0797bb457205d58620b1b0e69b5c782ef18
Nnaemeka Nnodim <mannynnodim@gmail.com>
2023-10-10 13:26:49 +0100

    update

--- app/(mentee)/mentee-booking/components/booking-session/Calender.tsx
+++ app/(mentee)/mentee-booking/components/booking-session/Calender.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { DayPicker } from "react-day-picker";
-import "react-day-picker/dist/style.css";
 
 const Calendarcomponent = () => {
   const [selectedDay, setSelectedDay] = useState<Date[]>();
