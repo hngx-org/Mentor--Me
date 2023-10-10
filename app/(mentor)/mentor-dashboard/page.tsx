18ba2d49af1455b18925dccf233fbc4b03b734d9
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-10-10 16:04:19 +0100

    add ability to switch from unverified to verified

--- app/(mentor)/mentor-dashboard/page.tsx
+++ app/(mentor)/mentor-dashboard/page.tsx
@@ -1,23 +1,23 @@
 "use client";
 
-import Link from "next/link";
 import React, { useState } from "react";
 import AboutMentor from "@/components/mentor-dashboard/AboutMentor";
 import MentorSession from "@/components/mentor-dashboard/MentorSession";
 import UnverifiedMentorCard from "@/components/mentor-dashboard/UnverifiedMentorCard";
-import Footer from "@/components/Footer";
-import MentorSideBar from "@/components/SideBar/MentorSideBar";
 import Modal from "@/components/mentor-dashboard/Modal";
-import MenteeNavBar from "@/components/menteeTopNav";
-import MobileSideBar from "@/components/mentor/MobileSiderBar";
 
 const page = () => {
-  const [isVerified, setIsVerified] = useState<boolean>(true);
+  const [isVerified, setIsVerified] = useState<boolean>(false);
   const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
     <div className="">
       <div className="px-5 py-10 lg:p-10 bg-[#f9fafc]">
-        {!isVerified && <UnverifiedMentorCard />}
+        {!isVerified && (
+          <UnverifiedMentorCard
+            isVerified={isVerified}
+            setIsVerified={setIsVerified}
+          />
+        )}
         <MentorSession
           isVerified={isVerified}
           isOpen={isOpen}
