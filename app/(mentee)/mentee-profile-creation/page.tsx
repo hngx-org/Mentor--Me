ff1adcc08479bdcbeb67546eeddbc5ab60683d42
Olaoluwa Emmanuel <116987000+Emmanuel0846@users.noreply.github.com>
2023-10-12 09:20:28 +0100

    Update page.tsx

--- app/(mentee)/mentee-profile-creation/page.tsx
+++ app/(mentee)/mentee-profile-creation/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-//  This component accepts 2 components : 1. MentorFormBuilder 2. MentorProgressBar
-// The MentorFormBuilder renders the forms for each screen, while accepting data from the formData.js file
-// The MentorProgressBar renders the progress bar in each screen based on a state variable called currForm
+//  This component accepts 2 components : 1. MenteeFormBuilder 2. MenteeProgressBar
+// The MenteeFormBuilder renders the forms for each screen, while accepting data from the formData.js file
+// The MenteeProgressBar renders the progress bar in each screen based on a state variable called currForm
 // The left side of the screen houses an svg element and a Container div which has the 5 forms. Each form is shown based on the state of currForm
 // The Container div has 5 divs, in which each div houses the heading, progressbar and the form itself
 
