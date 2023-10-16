b057c50f19a63fc04159c8bb0da481da75d1cebc
Vxrcel <skyeparagon@gmail.com>
2023-10-16 10:56:02 +0100

    how to fetch from cloudinary

--- app/example-api-images/page.tsx
+++ app/example-api-images/page.tsx
@@ -56,7 +56,7 @@ export default async function CloudinaryImages() {
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
         {results.resources.map((r) => (
-          <Suspense fallback={<Loading />}>
+          <Suspense key={r.public_id} fallback={<Loading />}>
             <div>
               <Image
                 key={r.public_id}
