38961b458eaf61d528587a2ed38f836c8679f168
Richard Eberechi Uche <eb3rechi@gmail.com>
2023-10-16 11:25:33 +0100

    ui changes

---
+++ components/cards/auth-profile-card/AuthProfileCard.tsx
@@ -0,0 +1,7 @@
+export default function AuthProfileCard() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
--- components/mentorProfile/MentorProfileHeader.tsx
+++ components/mentorProfile/MentorProfileHeader.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 
 import { ShareIcon } from "@/public/SVGs";
 import { Button } from "../buttons/button";
+import { ModalState } from "@/app/(mentor)/(dashboard-mentor)/mentor-profile/page";
 
 type MentorProfileHeaderProps = {
   userName: string;
@@ -12,6 +13,7 @@ type MentorProfileHeaderProps = {
   userRating: number;
   userId?: string;
   email?: string;
+  openModal: React.Dispatch<React.SetStateAction<ModalState>>;
 };
 
 export default function MentorProfileHeader({
@@ -20,6 +22,7 @@ export default function MentorProfileHeader({
   userRole,
   userId,
   email,
+  openModal,
 }: MentorProfileHeaderProps) {
   const [randomColor, setRandomColor] = useState("");
 
@@ -47,16 +50,21 @@ export default function MentorProfileHeader({
 
   return (
     <div className="w-[100%] h-[294px]   relative flex flex-col  ">
-      <div className="h-[50%] w-[100%]  bg-blue-500 bg-[url('/assets/mentor-profileBanner.png')] bg-cover bg-no-repeat" />
+      <div className="h-[50%] w-[100%]  bg-gradient-to-r from-fuchsia-500 via-red-600 to-orange-400  bg-cover bg-no-repeat" />
       <div className="h-[50%] w-[100%]  bg-white" />
-      <div className=" w-[80%]   h-fit flex  flex-col items-center space-y-2 sm:space-x-2  sm:flex-row self-center absolute  top-[100px] sm:top-[80px] ">
-        <div className=" w-[90px] h-[90px] sm:w-[200px] sm:h-[200px]   ">
+      <div className=" w-[80%]   h-fit flex  flex-col items-center space-y-2 sm:space-x-2  sm:flex-row self-center absolute  top-[100px] sm:top-[90px] ">
+        <div className=" w-[90px] h-[90px]  sm:w-fit sm:h-fit  border rounded-full border-[6px] border-white ">
           <Image
             src={`https://api.dicebear.com/7.x/initials/png?seed=${userName}`}
-            width={180}
-            height={180}
-            style={{ objectFit: "contain", borderRadius: "9999px" }}
+            width={200}
+            height={200}
+            sizes="500px"
+            style={{
+              objectFit: "cover",
+              borderRadius: "9999px",
+            }}
             alt="ths profileImage"
+            quality={100}
           />
         </div>
         <div className=" w-[100%] flex flex-col items-center sm:flex-row sm:justify-between px-2 ">
@@ -73,6 +81,12 @@ export default function MentorProfileHeader({
               variant="outline-primary"
               paddingLess
               className="px-1  sm:px-2 sm:py-2 "
+              onClick={() => {
+                openModal({
+                  isOpen: true,
+                  state: "basic info",
+                });
+              }}
             >
               Edit profile
             </Button>
