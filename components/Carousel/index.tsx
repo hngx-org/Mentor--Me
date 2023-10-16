54a0696d10332230b74c8676107d73fb315f59e0
goodness <goodyc474@gmail.com>
2023-10-16 03:47:25 +0100

    feat: complete carousel functionality

--- components/Carousel/index.tsx
+++ components/Carousel/index.tsx
@@ -4,40 +4,28 @@ import React from "react";
 import Image from "next/image";
 import LeftArrow from "@/public/assets/Icons/landing-page/arrow-circle-left.png";
 import RightArrow from "@/public/assets/Icons/landing-page/arrow-circle-right.png";
-import MentorImageOne from "@/public/assets/images/landing-page/mentorTypes-1.png";
-import MentorImageTwo from "@/public/assets/images/landing-page/mentorTypes-2.png";
-import MentorImageThree from "@/public/assets/images/landing-page/mentorTypes-3.png";
-import MentorImageFour from "@/public/assets/images/landing-page/mentorTypes-4.png";
 
-export default function Carousel({ imgCarousel }: any) {
+export default function Carousel({ imgCarousel, title, text }: any) {
   const [currentIndex, setCurrentIndex] = React.useState(0);
 
   const handleNext = () => {
-    setCurrentIndex(
-      imgCarousel.length > 1 ? currentIndex + 1 : imgCarousel.length - 3
-    );
-    console.log(currentIndex, imgCarousel.length);
+    setCurrentIndex(currentIndex === 2 ? 0 : currentIndex + 1);
   };
 
   const handlePrev = () => {
-    setCurrentIndex(
-      (currentIndex - 1 + imgCarousel.length) % imgCarousel.length
-    );
-    // console.log(currentIndex);
+    setCurrentIndex(currentIndex < 0 ? 0 : currentIndex - 1);
+    console.log(currentIndex);
   };
-  //   const translateX = -currentIndex * 100;
+
   return (
     <section>
-      <div className="md:flex mb-[8rem] md:mb-0 justify-between items-center">
+      <div className="md:flex mb-[8rem] md:mb-0 justify-between items-center gap-[40px]">
         <div className="w-full md:w-1/3 mx-auto max-w-[426px] mb-[5rem] md:mb-[8rem]">
           <h3 className="text-[1.2rem] text-center md:text-left md:text-[1.6rem] font-semibold">
-            Tech Experienced Mentors
+            {title}
           </h3>
           <p className="text-[0.85rem] text-center md:text-left md:text-[0.9rem] py-[1rem]">
-            Welcome to Mentor.Me, your gateway to a world of tech excellence and
-            innovation. We believe that mentorship is the cornerstone of
-            personal and professional growth in the fast-paced and ever-evolving
-            world of technology.
+            {text}
           </p>
 
           <div className="w-[144px] mx-auto md:mx-0 xl:mx-0 flex justify-between mt-[1rem] md:mt-[2rem]">
@@ -61,20 +49,22 @@ export default function Carousel({ imgCarousel }: any) {
           </div>
         </div>
 
-        <div className="flex relative justify-center items-center overflow-hidden w-[80%] ease-in-out translate-x-0">
+        <div className="flex relative justify-center items-center overflow-hidden w-[100%] ease-in-out translate-x-0">
           {imgCarousel.map((item: any, index: number) => (
             <div
               style={{ transform: `translateX(${-currentIndex}00%` }}
-              className={`block w-3/4 mx-auto xl:mx-0 max-w-[800px] mb-[2rem] xl:mb-0 xl:ml-[4rem]  xl:flex items-center xl:justify-between transition-transform gap-20  
-              
-              }`}
+              key={index}
+              // className={`block w-3/4 mx-auto xl:mx-0 max-w-[800px] mb-[2rem] xl:mb-0 xl:ml-[4rem]  xl:flex items-center xl:justify-between transition-transform gap-20
+
+              // }`}
+              className="flex justify-center gap-3 ease-in-out"
             >
               <Image
                 src={item}
                 alt=""
                 width={500}
                 height={200}
-                className="block mb-[2rem] mx-auto ease-in-out duration-300 xl:max-w-[300px] max-w-[200px]"
+                className="block mb-[2rem] mx-auto ease-in-out duration-300 xl:max-w-[250px] max-w-[200px] p-2"
               />
             </div>
           ))}
