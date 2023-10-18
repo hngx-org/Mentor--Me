febabb20495e346ad81396360241ba514d6aeb92
zua <killthelast01@gmail.com>
2023-10-18 20:04:37 +0100

    feat: cleanup community pages structure and routing

---
+++ components/Community/index.ts
@@ -0,0 +1,11 @@
+export { default as MentorGrid } from "./MentorGrid";
+export { default as MentorCard } from "./MentorCard";
+export { default as BigMentorshipCard } from "./BigMentorShipCard";
+
+export { default as CommunitySearchBar } from "../Community/searchcommunity-searchbar";
+export { default as ForumCard } from "../Community/ForumCard";
+export { default as StartDiscussionModal } from "../Community/startdiscussion-modal";
+export { default as Discussion } from "../Community/discussion-card";
+export { default as DiscussionForums } from "../Community/discussion-forums";
+export { default as MentorSeshCard } from "../Community/mentorshipsession-card";
+export { default as MentorshipSessions } from "../Community/mentorship-sessions";
