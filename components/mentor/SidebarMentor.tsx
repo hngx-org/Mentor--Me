de103b54aebd99660023207798d6d71254a44863
Vxrcel <skyeparagon@gmail.com>
2023-10-18 12:02:46 +0100

    Isaac Gideon

--- components/mentor/SidebarMentor.tsx
+++ components/mentor/SidebarMentor.tsx
@@ -26,13 +26,14 @@ export type SideBarMentorProps = {
   jobTitle?: string | null;
 };
 
-export default async function SidebarMentor({
+export default function SidebarMentor({
   light,
   path,
   imgSrc,
   name,
   bio,
   email,
+  jobTitle,
 }: SideBarMentorProps & { path?: string | null | undefined }) {
   const { user } = useAuthCtx();
 
